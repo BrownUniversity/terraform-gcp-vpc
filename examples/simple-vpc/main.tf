04e528e284ae08b66c814c457b6a284f2a37fa06
Isabel Restrepo <isabelrestre@gmail.com>
2021-06-09 22:03:45 -0400

    build: update versions, point project module to feature branch f

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "simple-project" {
-  source          = "git::https://github.com/BrownUniversity/terraform-gcp-project.git?ref=v0.1.0"
+  source          = "git::https://github.com/BrownUniversity/terraform-gcp-project.git?ref=feat-up-major-versions"
   project_name    = "inspec-vpc"
   folder_id       = var.folder_id
   billing_account = var.billing_account
