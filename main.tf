f4787a0e9dde745526ae0146c7883446331be6a9
Paul Xu <yang_xu@brown.edu>
2024-07-18 17:00:57 -0400

    Update dependency versions

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create a simple VPC, Subnets and Secondary Ranges
 module "vpc" {
   source  = "terraform-google-modules/network/google"
-  version = "7.0.0"
+  version = "9.1.0"
 
   project_id   = var.project_id
   network_name = var.network_name
